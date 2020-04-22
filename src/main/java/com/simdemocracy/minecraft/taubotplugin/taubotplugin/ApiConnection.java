6cb2b8a65f332d5df5ec40f5a8a6283de12f835a
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-04-22 23:29:24 +0100

    added comments

--- src/main/java/com/simdemocracy/minecraft/taubotplugin/taubotplugin/ApiConnection.java
+++ src/main/java/com/simdemocracy/minecraft/taubotplugin/taubotplugin/ApiConnection.java
@@ -9,7 +9,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
-
+//exception
 class ApiException extends Exception {
     public ApiException(String errorMessage) {
         super(errorMessage);
@@ -24,6 +24,8 @@ public class ApiConnection {
     String ip;
 
     public ApiConnection(){
+        //Builds connection from config
+        //should probably be moved to its own function
         System.out.println("Building config");
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try(FileReader fileReader = new FileReader("config.json")) {
@@ -46,7 +48,7 @@ public class ApiConnection {
 
 
     private String Get(String toUrl) throws ApiException {
-
+        //Simple connection to taubot api
         try {
             URL url = new URL (toUrl);
             String encoding = Base64.getEncoder().encodeToString((this.uname + ":" + this.psswd).getBytes("utf-8"));
@@ -62,16 +64,18 @@ public class ApiConnection {
                 return line;
             }
         } catch(Exception e) {
-            //e.printStackTrace();
+            
             throw new ApiException("Failed to connect");
         }
         throw new ApiException("wtf have you done you magical cow");
 
     }
     public int getUserBalance(String ign) throws ApiException {
+        //simple balance getter
         String balance = Get("http://"+ this.ip +"/Minecraft/account/" + ign + "/balance");
         String[] content = balance.split(" ");
         if (content.length > 1) {
+            //sloppy code I know but it works
             throw new ApiException("Failed with error code: " + content[0]);
         }
         return Integer.parseInt(balance);
